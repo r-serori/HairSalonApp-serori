b141820361f36e57543045749d2e36de49c1da17
r-serori <ryunosuke.sakaue@gmail.com>
2024-08-01 16:40:17 +0900

    next.config.mjs編集

--- hair-salon-frontend-serori/next.config.mjs
+++ hair-salon-frontend-serori/next.config.mjs
@@ -1,6 +1,20 @@
+import { NextConfig } from 'next';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`, // LaravelのAPIエンドポイント
+      },
+      {
+        source: '/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`, // Laravelのエンドポイント
+      },
+    ];
+  },
 };
 
 export default nextConfig;
