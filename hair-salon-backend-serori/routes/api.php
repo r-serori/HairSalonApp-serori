cb2fc5477bdd624a02daf3df6062af9a25282d8a
r-serori <155217702+r-serori@users.noreply.github.com>
2024-08-01 13:49:35 +0900

    Update api.php

--- hair-salon-backend-serori/routes/api.php
+++ hair-salon-backend-serori/routes/api.php
@@ -8,12 +8,6 @@ use Illuminate\Support\Facades\Http;
 Route::middleware('api')->group(
   function () {
 
-    Route::get('/sanctum/csrf-cookie', function () {
-      return response()->json([
-        'message' => 'CSRF Cookie Set'
-      ], 200, [], JSON_UNESCAPED_UNICODE)->header('Content-Type', 'application/json; charset=UTF-8');
-    });
-
     Route::get('/search/{zipCode}', function ($code) {
 
       $decodedCode = urldecode($code);
