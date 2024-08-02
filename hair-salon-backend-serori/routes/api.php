0ad6110667c63210d1701220f4e4cdd5694f11ed
r-serori <ryunosuke.sakaue@gmail.com>
2024-08-03 00:48:00 +0900

    web.php edit

--- hair-salon-backend-serori/routes/api.php
+++ hair-salon-backend-serori/routes/api.php
@@ -5,24 +5,20 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
 
-Route::middleware('api')->group(
-  function () {
 
-    Route::get('/sanctum/csrf-cookie', function () {
-      return response()->json([
-        'message' => 'CSRF Cookie Set'
-      ], 200, [], JSON_UNESCAPED_UNICODE)->header('Content-Type', 'application/json; charset=UTF-8');
-    });
+Route::get('/sanctum/csrf-cookie', function () {
+  return response()->json([
+    'message' => 'CSRF Cookie Set'
+  ], 200, [], JSON_UNESCAPED_UNICODE)->header('Content-Type', 'application/json; charset=UTF-8');
+});
 
-    Route::get('/search/{zipCode}', function ($code) {
+Route::get('/search/{zipCode}', function ($code) {
 
-      $decodedCode = urldecode($code);
+  $decodedCode = urldecode($code);
 
-      $response = Http::get('https://zipcloud.ibsnet.co.jp/api/search', [
-        'zipcode' => $decodedCode,
-      ]);
+  $response = Http::get('https://zipcloud.ibsnet.co.jp/api/search', [
+    'zipcode' => $decodedCode,
+  ]);
 
-      return response()->json($response->json());
-    });
-  }
-);
+  return response()->json($response->json());
+});
