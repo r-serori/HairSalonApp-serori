2a08c439d0771beb2f6cbb1989b55dd186213b5a
r-serori <ryunosuke.sakaue@gmail.com>
2024-08-03 11:28:35 +0900

    cors??

--- hair-salon-backend-serori/app/Providers/RouteServiceProvider.php
+++ hair-salon-backend-serori/app/Providers/RouteServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
@@ -28,15 +28,14 @@ class RouteServiceProvider extends ServiceProvider
   {
     $this->configureRateLimiting();
 
-    $this->routes(function () {
-      Route::middleware('api')
-        ->prefix('api')
-        ->group(base_path('routes/api.php'));
+    Route::middleware('api')
+      ->prefix('api')
+      ->group(base_path('routes/api.php'));
 
-      Route::middleware('web')
-        ->group(base_path('routes/web.php'));
-    });
+    Route::middleware('web')
+      ->group(base_path('routes/web.php'));
   }
+
 
   /**
    * Configure the rate limiters for the application.
