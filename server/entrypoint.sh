f7f53a1d1f03c2b4208597dcfa782643226a9324
r-serori <ryunosuke.sakaue@gmail.com>
2024-08-07 23:03:01 +0900

    herokuは権限を指定できない？？

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -9,14 +9,14 @@ sed -i -e "s/<VirtualHost \*:80>/<VirtualHost \*:${PORT}>/g" /etc/apache2/sites-
 #     sed -i -e "s/<VirtualHost \*:80>/<VirtualHost \*:8000>/g" /etc/apache2/sites-available/000-default.conf 
 
 # 権限を設定
-chown -R www-data:www-data /var/www/html/public /var/www/html/public/storage /var/www/html/storage/app/public /var/www/html/storage/app/public/startPhotos /var/www/html/storage/app/public/endPhotos 
-chmod -R 755 /var/www/html/public
-chmod -R 755 /var/www/html/public/storage
-chmod -R 755 /var/www/html/storage
-chmod -R 755 /var/www/html/storage/app
-chmod -R 755 /var/www/html/storage/app/public　
-find /var/www/html/storage/app/public/startPhotos -type f -exec chmod 644 {} \;
-find /var/www/html/storage/app/public/endPhotos -type f -exec chmod 644 {} \;
+# chown -R www-data:www-data /var/www/html/public /var/www/html/public/storage /var/www/html/storage/app/public /var/www/html/storage/app/public/startPhotos /var/www/html/storage/app/public/endPhotos 
+# chmod -R 755 /var/www/html/public
+# chmod -R 755 /var/www/html/public/storage
+# chmod -R 755 /var/www/html/storage
+# chmod -R 755 /var/www/html/storage/app
+# chmod -R 755 /var/www/html/storage/app/public　
+# find /var/www/html/storage/app/public/startPhotos -type f -exec chmod 644 {} \;
+# find /var/www/html/storage/app/public/endPhotos -type f -exec chmod 644 {} \;
 
 # 既存のシンボリックリンクやディレクトリを削除
 rm -rf /var/www/html/public/storage
